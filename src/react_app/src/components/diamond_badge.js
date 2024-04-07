eda0e4dde95bf280cfe95dda3b2d117ac1d24e19
Cesar E Garza <cesar@cegarza.com>
2024-04-07 01:31:41 -0500

    glow effects look nicer now

--- src/react_app/src/components/diamond_badge.js
+++ src/react_app/src/components/diamond_badge.js
@@ -2,20 +2,19 @@ import React from "react";
 import DiamondBadgeIcon from "../assets/icons/sorder.png";
 
 const DiamondBadge = ({ count }) => {
-  const badgeClasses = `badge-container h-10 w-10 ${
+  const badgeClasses = `badge-container badge-rainbow h-10 w-10 ${
     count > 1 ? "has-count" : ""
   }`;
-  const shineClasses = `badge-shine ${count >= 10 ? "high-value" : ""}`;
+  const badgeShineClasses = `badge-image high-value`;
 
   return (
     <div className={badgeClasses}>
       <img
         src={DiamondBadgeIcon}
         alt="Achived 4 Top 10s in one season Badge"
-        className="badge-image"
+        className={badgeShineClasses}
       />
       {count > 1 && <span className="badge-count">x{count}</span>}
-      <div className={shineClasses}></div>
     </div>
   );
 };
--- src/react_app/src/components/top500_badge.js
+++ src/react_app/src/components/top500_badge.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 import Top500BadgeIcon from "../assets/icons/t500.png";
 
 const Top500Badge = ({ count }) => {
-    const badgeClasses = `badge-container h-10 w-10 ${count > 1 ? 'has-count' : ''}`;
-    const shineClasses = `badge-shine ${count >= 500 ? 'high-value' : ''}`;
-    
-    return (
-        <div className={badgeClasses}>
-        <img src={Top500BadgeIcon} alt="Top 500 Badge" className="badge-image"/>
-        {count > 1 && (
-            <span className="badge-count">x{count}</span>
-        )}
-        <div className={shineClasses}></div>
-        </div>
-    );
-    }
+  const badgeClasses = `badge-container badge-silver h-10 w-10 ${
+    count > 1 ? "has-count" : ""
+  }`;
+  const badgeShineClasses = `badge-image ${count >= 20 ? "high-value" : ""}`;
+
+  return (
+    <div className={badgeClasses}>
+      <img
+        src={Top500BadgeIcon}
+        alt="Top 500 Badge"
+        className={badgeShineClasses}
+      />
+      {count > 1 && <span className="badge-count">x{count}</span>}
+    </div>
+  );
+};
 
 export default Top500Badge;
