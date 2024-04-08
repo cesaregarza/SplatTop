fb52a29774771728009d399a5aae08deb59701ab
Cesar E Garza <cesar@cegarza.com>
2024-04-07 23:32:40 -0500

    Add About page component

--- src/react_app/src/App.js
+++ src/react_app/src/App.js
@@ -4,6 +4,7 @@ import Top500 from './components/top500';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import FAQ from './components/faq';
+import About from './components/about';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Top500 />} />
           <Route exact path="/faq" element={<FAQ />} />
+          <Route exact path="/about" element={<About />} />
           {/* Add more routes for other components */}
         </Routes>
         <Footer />
