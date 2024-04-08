9e8a3d3ebb9eeacc54bde80b7426cede7e33bf76
Cesar E Garza <cesar@cegarza.com>
2024-04-07 23:04:12 -0500

    Add FAQ component and route

--- src/react_app/src/App.js
+++ src/react_app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Top500 from './components/top500';
 import Navbar from './components/navbar';
+import FAQ from './components/faq';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Top500 />} />
+          <Route exact path="/faq" element={<FAQ />} />
           {/* Add more routes for other components */}
         </Routes>
       </div>
