9dc626b5e1d1e5f78215854d686bdd67de1a2a8e
Cesar E Garza <cesar@cegarza.com>
2024-04-14 22:25:25 -0500

    improved the color switch

--- src/react_app/src/components/player_components/xchart.js
+++ src/react_app/src/components/player_components/xchart.js
@@ -209,7 +209,7 @@ class XChart extends React.Component {
           <span className="text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">
             Classic
           </span>
-          <div className="relative">
+          <div className="relative" title="Change the color scheme">
             <input
               type="checkbox"
               id="toggleColorMode"
@@ -218,11 +218,11 @@ class XChart extends React.Component {
               onChange={this.toggleColorMode}
             />
             <div
-              className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:after:translate-x-5"
-              style={{
-                backgroundColor:
-                  this.state.colorMode === "Seasonal" ? "#3b82f6" : "#ab5ab7",
-              }}
+              className={`w-11 h-6 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:after:translate-x-5 ${
+                this.state.colorMode === "Seasonal"
+                  ? "seasonal-bg"
+                  : "classic-bg"
+              }`}
             ></div>
           </div>
           <span className="text-sm font-medium text-gray-900 dark:text-gray-300 ml-2">
