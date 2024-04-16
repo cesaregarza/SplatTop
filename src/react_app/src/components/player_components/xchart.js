1d5c68675bb8a0231a3e841159340d46a088521e
Cesar E Garza <cesar@cegarza.com>
2024-04-16 00:42:22 -0500

    fixed button getting cut off

--- src/react_app/src/components/player_components/xchart.js
+++ src/react_app/src/components/player_components/xchart.js
@@ -212,33 +212,35 @@ class XChart extends React.Component {
 
     return (
       <div className="xchart-container">
-        <label
-          htmlFor="toggleColorMode"
-          className="inline-flex items-center justify-center w-full cursor-pointer"
-        >
-          <span className="text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">
-            Classic
-          </span>
-          <div className="relative" title="Change the color scheme">
-            <input
-              type="checkbox"
-              id="toggleColorMode"
-              className="sr-only peer"
-              checked={this.state.colorMode === "Seasonal"}
-              onChange={this.toggleColorMode}
-            />
-            <div
-              className={`w-11 h-6 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:after:translate-x-5 ${
-                this.state.colorMode === "Seasonal"
-                  ? "seasonal-bg"
-                  : "classic-bg"
-              }`}
-            ></div>
-          </div>
-          <span className="text-sm font-medium text-gray-900 dark:text-gray-300 ml-2">
-            Seasonal
-          </span>
-        </label>
+        <div className="pb-4 flex justify-center">
+          <label
+            htmlFor="toggleColorMode"
+            className="inline-flex items-center cursor-pointer"
+          >
+            <span className="text-sm font-medium text-gray-900 dark:text-gray-300 mr-2">
+              Classic
+            </span>
+            <div className="relative" title="Change the color scheme">
+              <input
+                type="checkbox"
+                id="toggleColorMode"
+                className="sr-only peer"
+                checked={this.state.colorMode === "Seasonal"}
+                onChange={this.toggleColorMode}
+              />
+              <div
+                className={`w-11 h-6 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:after:translate-x-5 ${
+                  this.state.colorMode === "Seasonal"
+                    ? "seasonal-bg"
+                    : "classic-bg"
+                }`}
+              ></div>
+            </div>
+            <span className="text-sm font-medium text-gray-900 dark:text-gray-300 ml-2">
+              Seasonal
+            </span>
+          </label>
+        </div>
         <HighchartsReact highcharts={Highcharts} options={options} />
       </div>
     );
