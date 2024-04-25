14c0aa9a14fb28278afebccda17cde5dc8c26ad3
Cesar E Garza <cesar@cegarza.com>
2024-04-24 22:40:12 -0500

    aliases look better

--- src/react_app/src/components/player_components/aliases.js
+++ src/react_app/src/components/player_components/aliases.js
@@ -2,13 +2,15 @@ import React from "react";
 
 const Aliases = ({ data }) => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">Player Names:</h2>
+    <div className="w-full">
+      <h2 className="text-2xl font-bold mb-4">Aliases</h2>
       <table className="w-full text-left alias-table border border-gray-300">
         <thead>
           <tr className="border-b-4 border-gray-400">
-            <th className="px-4 py-2 bg-gray-700">Splashtag</th>
-            <th className="px-4 py-2 bg-gray-700">Last Seen</th>
+            <th className="w-1/2 px-4 py-2 bg-gray-700 text-left">Splashtag</th>
+            <th className="w-1/2 px-4 py-2 bg-gray-700 text-right">
+              Last Seen
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -24,16 +26,23 @@ const Aliases = ({ data }) => {
             const namePart = splashtag.substring(0, splitIndex);
             const tagPart = splashtag.substring(splitIndex);
             return (
-              <tr key={`${splashtag}-${index}`} className="border-b border-gray-200">
-                <td className="px-4 py-2">
-                  <span className="text-purplelight">{namePart}</span>{tagPart}
+              <tr
+                key={`${splashtag}-${index}`}
+                className="border-b border-gray-200"
+              >
+                <td className="w-1/2 px-4 py-2 text-left">
+                  <span className="text-purplelight">{namePart}</span>
+                  {tagPart}
                 </td>
-                <td className="px-4 py-2">
-                  {new Date(latest_updated_timestamp).toLocaleString("default", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit",
-                  })}
+                <td className="w-1/2 px-4 py-2 text-right">
+                  {new Date(latest_updated_timestamp).toLocaleString(
+                    "default",
+                    {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }
+                  )}
                 </td>
               </tr>
             );
