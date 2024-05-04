dea636d9576e0ea7b10f6693fdb5e15e8d8f802f
Cesar E Garza <cesar@cegarza.com>
2024-05-03 23:56:52 -0500

    Refactor aliases table styling

--- src/react_app/src/components/player_components/aliases.jsx
+++ src/react_app/src/components/player_components/aliases.jsx
@@ -2,26 +2,17 @@ import React from "react";
 
 const Aliases = ({ data }) => {
   return (
-    <div className="w-full">
-      <h2 className="text-2xl font-bold mb-4">Aliases</h2>
-      <table className="w-full text-left alias-table border border-gray-300">
+    <div className="aliases-container mx-auto w-full">
+      <h2 className="aliases-title text-2xl font-bold mb-4">Aliases</h2>
+      <table className="aliases-table w-full border-collapse">
         <thead>
-          <tr className="border-b-4 border-gray-400">
-            <th className="w-1/2 px-4 py-2 bg-gray-700 text-left">Splashtag</th>
-            <th className="w-1/2 px-4 py-2 bg-gray-700 text-right">
-              Last Seen
-            </th>
+          <tr className="bg-gray-800 text-white">
+            <th className="px-4 py-2 text-right alias-header">Splashtag</th>
+            <th className="px-4 py-2 text-left alias-header">Last Seen</th>
           </tr>
         </thead>
         <tbody>
-          {[
-            ...new Set(
-              data.map(({ splashtag, latest_updated_timestamp }) => ({
-                splashtag,
-                latest_updated_timestamp,
-              }))
-            ),
-          ].map(({ splashtag, latest_updated_timestamp }, index) => {
+          {data.map(({ splashtag, latest_updated_timestamp }, index) => {
             const splitIndex = splashtag.search(/#\d{4}[0-9a-f]?$/);
             const namePart = splashtag.substring(0, splitIndex);
             const tagPart = splashtag.substring(splitIndex);
@@ -30,11 +21,11 @@ const Aliases = ({ data }) => {
                 key={`${splashtag}-${index}`}
                 className="border-b border-gray-200"
               >
-                <td className="w-1/2 px-4 py-2 text-left">
-                  <span className="text-purplelight">{namePart}</span>
-                  {tagPart}
+                <td className="px-4 py-2 flex justify-end items-center text-right">
+                  <span className="mr-2 text-purplelight">{namePart}</span>
+                  <span className="font-medium">{tagPart}</span>
                 </td>
-                <td className="w-1/2 px-4 py-2 text-right">
+                <td className="px-4 py-2 text-left">
                   {new Date(latest_updated_timestamp).toLocaleString(
                     "default",
                     {
