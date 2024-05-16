53a1091d95dd7e0066321143b06669736f437f93
Cesar E Garza <cesar@cegarza.com>
2024-05-15 22:24:46 -0500

    converted main page title

--- src/react_app/src/components/top500_components/columns/badges_column.js
+++ src/react_app/src/components/top500_components/columns/badges_column.js
@@ -10,43 +10,46 @@ const Top10BadgeMemo = React.memo(Top10Badge);
 const Top500BadgeMemo = React.memo(Top500Badge);
 
 export const id = "badges";
-export const name = "Badges";
+export const title_key = "column_badge_title";
 export const isVisible = true;
 export const headerClasses = "flex-grow px-4 py-2 text-center";
 export const cellClasses = "flex-grow px-4 py-2 text-center";
-export const render = (player) => (
-  <div className="flex flex-col sm:flex-row justify-center items-center gap-2">
-    <div className="flex flex-wrap justify-center items-center gap-2">
-      <div className="min-w-[40px]">
-        <img
-          src={player.prev_season_region ? TakorokaIcon : TentatekIcon}
-          alt={`Player was in ${
-            player.prev_season_region ? "Takoroka" : "Tentatek"
-          } last season`}
-          className={`h-10 w-10 object-cover aspect-square`}
-        />
-      </div>
-      <div className="min-w-[40px]">
-        {player.diamond_x_count > 0 ? (
-          <DiamondBadgeMemo count={player.diamond_x_count} />
-        ) : (
-          <div className="h-10 w-10 invisible"></div>
-        )}
-      </div>
-      <div className="min-w-[40px]">
-        {player.gold_x_count > 0 ? (
-          <Top10BadgeMemo count={player.gold_x_count} />
-        ) : (
-          <div className="h-10 w-10 invisible"></div>
-        )}
-      </div>
-      <div className="min-w-[40px]">
-        {player.silver_x_count > 0 ? (
-          <Top500BadgeMemo count={player.silver_x_count} />
-        ) : (
-          <div className="h-10 w-10 invisible"></div>
-        )}
+export const render = (player, t) => {
+  const prev_season_key = player.prev_season_region ?
+    "player_was_in_takoroka_last_season" :
+    "player_was_in_tentatek_last_season";
+  return (
+    <div className="flex flex-col sm:flex-row justify-center items-center gap-2">
+      <div className="flex flex-wrap justify-center items-center gap-2">
+        <div className="min-w-[40px]">
+          <img
+            src={player.prev_season_region ? TakorokaIcon : TentatekIcon}
+            alt={t(prev_season_key)}
+            className={`h-10 w-10 object-cover aspect-square`}
+          />
+        </div>
+        <div className="min-w-[40px]">
+          {player.diamond_x_count > 0 ? (
+            <DiamondBadgeMemo count={player.diamond_x_count} />
+          ) : (
+            <div className="h-10 w-10 invisible"></div>
+          )}
+        </div>
+        <div className="min-w-[40px]">
+          {player.gold_x_count > 0 ? (
+            <Top10BadgeMemo count={player.gold_x_count} />
+          ) : (
+            <div className="h-10 w-10 invisible"></div>
+          )}
+        </div>
+        <div className="min-w-[40px]">
+          {player.silver_x_count > 0 ? (
+            <Top500BadgeMemo count={player.silver_x_count} />
+          ) : (
+            <div className="h-10 w-10 invisible"></div>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/react_app/src/components/top500_components/columns/splashtag_column.js
+++ src/react_app/src/components/top500_components/columns/splashtag_column.js
@@ -1,6 +1,6 @@
 export const id = "splashtag";
-export const name = "Splashtag";
+export const title_key = "column_splashtag_title";
 export const isVisible = true;
 export const headerClasses = "flex-grow px-4 py-2 text-center";
 export const cellClasses = "flex-grow px-4 py-2 text-center";
-export const render = (player) => player.splashtag;
+export const render = (player, t) => player.splashtag;
