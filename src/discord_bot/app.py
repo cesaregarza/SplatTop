03453e39bcd507a3862b17bf50ad008661c8adbb
Cesar E Garza <cesar@cegarza.com>
2024-07-02 01:27:18 -0500

    formatting

--- src/discord_bot/app.py
+++ src/discord_bot/app.py
@@ -10,9 +10,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     logger.info(f"Logged in as {client.user}")
+
 
 @client.event
 async def on_message(message):
@@ -21,5 +23,6 @@ async def on_message(message):
 
     if message.content.startswith("!hello"):
         await message.channel.send("Hello!")
+
 
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
